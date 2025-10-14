2bbe477258ad89c0ff8bd705676288b83b966de5
armanarstambekov <armanarstambekov@gmail.com>
2025-10-14 09:48:57 +0500

    test

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,12 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,  # разрешаем NULL
+        blank=True,  # разрешаем пустое в формах
+        on_delete=models.SET_NULL  # если пользователь удалён — ставим NULL
+    )
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
